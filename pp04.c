1c9a14d9941d570d6da869e720ffcd9dffd50c52
Shahriar Faysal <160359045+shahriarfaysal@users.noreply.github.com>
2024-02-22 22:33:40 +0600

    Add files via upload

---
+++ pp04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+   int num;
+
+   printf("Enter an integer: ");
+   scanf("%d", &num);
+
+   if (num > 0)
+   {
+     printf("Positive\n");
+   }
+    else if (num < 0)
+    {
+        printf("Negative\n");
+    }
+    else
+    {
+        printf("Zero");
+    }
+    return 0;
+}
